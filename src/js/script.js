901a75e659020c7f8d10e2b754e77e9c9a5c2aab
DartKomashi <dartkomashi@gmail.com>
2023-05-12 02:29:38 +0300

    second lesson

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        autoplay: true,
-        autoplaySpeed: 2000,
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron-left-solid.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-right-solid.svg"></button>',
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              dots: true,
+              arrows: false
+            }
+          }
+        ]
       });
 });
