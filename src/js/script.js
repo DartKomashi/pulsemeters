28f1a200a9b83800e7d8ac5027af8084456d7fb8
DartKomashi <dartkomashi@gmail.com>
2023-05-12 23:32:07 +0300

    jedem das seine

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
         speed: 1200,
+        infinite: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron-left-solid.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-right-solid.svg"></button>',
         responsive: [
