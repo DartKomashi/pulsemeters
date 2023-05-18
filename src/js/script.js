782d593efbd5ee07b49acfe571d3661963c17889
DartKomashi <dartkomashi@gmail.com>
2023-05-19 02:00:10 +0300

    self made

--- src/js/script.js
+++ src/js/script.js
@@ -24,19 +24,16 @@ $(document).ready(function(){
           .index()).addClass('catalog__content_active');
       });
 
-      $('.catalog-item__link').each(function(i) {
-        $(this).on('click', function(e) {
-          e.preventDefault();
-          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      function toggleSlide(item) {
+        $(item).each(function(i) {
+          $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          })
         })
-      })
+      }
 
-      $('.catalog-item__back').each(function(i) {
-        $(this).on('click', function(e) {
-          e.preventDefault();
-          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-        })
-      })
+      toggleSlide ('.catalog-item__link');
+      toggleSlide ('.catalog-item__back');
 });
