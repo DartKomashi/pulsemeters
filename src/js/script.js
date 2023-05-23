b7aa389efa9084d34ece7485257e440610cec7cf
DartKomashi <dartkomashi@gmail.com>
2023-05-24 01:54:10 +0300

    modal

--- src/js/script.js
+++ src/js/script.js
@@ -45,4 +45,11 @@ $(document).ready(function(){
       $('.modal__close').on('click', function() {
         $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
       });
+      
+      $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn('slow');
+        })
+      });
 });
