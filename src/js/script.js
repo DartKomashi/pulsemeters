2f758fb24af1a53646b3ece6f042708f4cc05b3b
DartKomashi <dartkomashi@gmail.com>
2023-05-25 00:18:48 +0300

    valid

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+      //slider
+
     $('.carousel__inner').slick({
         autoplay: true,
         autoplaySpeed: 2000,
@@ -17,12 +20,16 @@ $(document).ready(function(){
         ]
       });
 
+      //tabs
+
       $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this)
           .index()).addClass('catalog__content_active');
       });
+
+      //catalig-items
 
       function toggleSlide(item) {
         $(item).each(function(i) {
@@ -52,4 +59,35 @@ $(document).ready(function(){
           $('.overlay, #order').fadeIn('slow');
         })
       });
+
+      //validation
+
+      function valideForms(form){
+        $(form).validate({
+          rules: {
+            name: "required",
+            phone: "required",
+            email: {
+              required: true,          
+              email: true
+            }
+          },
+          messages: {
+            name: "Пожалуйста, введите Ваше имя",
+            phone: "Пожалуйста, введите номер телефона",
+            email: {
+              required: "Пожалуйста, введите Ваш email",
+              email: "Проверьте правильность ввода email"
+            }
+          }
+        });
+      };
+
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
+
+    //masked
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 });
